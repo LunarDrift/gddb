ee1f08ad7cf8e4f872fbe5c976f4100b875b9590
Travis <mynameismyname1992@gmail.com>
2026-06-14 22:39:21 -0500

    Update dependencies. Run importer

--- cmd/import/main.go
+++ cmd/import/main.go
@@ -6,9 +6,16 @@ import (
 	"os"
 
 	"github.com/LunarDrift/deadabase/internal/importer"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Couldn't find environment file")
+	}
+
 	connStr := os.Getenv("DB_URL")
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
