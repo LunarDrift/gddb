b812ef39a6d9a4e57bf56b08ce053a332d4f8f86
Travis <mynameismyname1992@gmail.com>
2026-07-04 10:04:20 -0500

    Add ShowQuerier interface to server.go. Includes all current sqlc-generated functions being used in the endpoints

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -1,17 +1,40 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"strings"
+	"time"
 
 	"github.com/LunarDrift/deadabase/internal/database"
 
 	_ "github.com/lib/pq"
 )
+
+type ShowQuerier interface {
+	GetAllShowIDs(ctx context.Context) ([]int32, error)
+	GetShowFromDate(ctx context.Context, showDate time.Time) ([]database.GetShowFromDateRow, error)
+	GetShowFromID(ctx context.Context, showID int32) ([]database.GetShowFromIDRow, error)
+	GetShowsBetweenDates(ctx context.Context, arg database.GetShowsBetweenDatesParams) ([]database.GetShowsBetweenDatesRow, error)
+	GetShowsFromSetName(ctx context.Context, setName string) ([]database.GetShowsFromSetNameRow, error)
+	GetShowsFromSongName(ctx context.Context, rawEntry string) ([]database.GetShowsFromSongNameRow, error)
+	GetShowsFromState(ctx context.Context, stateOrCountry string) ([]database.GetShowsFromStateRow, error)
+	GetShowsFromYear(ctx context.Context, year int32) ([]database.GetShowsFromYearRow, error)
+	GetShowsFromYearAndState(ctx context.Context, arg database.GetShowsFromYearAndStateParams) ([]database.GetShowsFromYearAndStateRow, error)
+	SearchByVenue(ctx context.Context, venue string) ([]database.SearchByVenueRow, error)
+	ShowsWithShowNotes(ctx context.Context) ([]database.ShowsWithShowNotesRow, error)
+	ShowsWithoutNotes(ctx context.Context) ([]database.ShowsWithoutNotesRow, error)
+	SongStats(ctx context.Context, songName sql.NullString) (database.SongStatsRow, error)
+	AllSongsPlayedAtVenue(ctx context.Context, venue string) ([]database.AllSongsPlayedAtVenueRow, error)
+	MostCommonSongsBySetName(ctx context.Context, setName string) ([]database.MostCommonSongsBySetNameRow, error)
+	MostPlayedSongs(ctx context.Context) ([]database.MostPlayedSongsRow, error)
+	SongsPlayedLessThan(ctx context.Context, dollar_1 interface{}) ([]database.SongsPlayedLessThanRow, error)
+	UniqueSongsPerCity(ctx context.Context) ([]database.UniqueSongsPerCityRow, error)
+}
 
 type server struct {
 	mux     *http.ServeMux
