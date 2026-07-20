860bb72d6c43328d64f2fa2939457bb944f61ddf
Travis <mynameismyname1992@gmail.com>
2026-07-19 20:30:07 -0500

    Add redactIP helper

--- cmd/api/middleware/logger.go
+++ cmd/api/middleware/logger.go
@@ -2,8 +2,10 @@
 package middleware
 
 import (
+	"fmt"
 	"io"
 	"log/slog"
+	"net"
 	"net/http"
 	"time"
 )
@@ -39,6 +41,21 @@ func (req *requestRecorder) Read(p []byte) (int, error) {
 	return n, err
 }
 
+func redactIP(addr string) string {
+	host, _, err := net.SplitHostPort(addr)
+	if err != nil {
+		host = addr
+	}
+	ip := net.ParseIP(host)
+	if ip == nil {
+		return host
+	}
+	if ip4 := ip.To4(); ip4 != nil {
+		return fmt.Sprintf("%d.%d.%d.x", ip4[0], ip4[1], ip4[2])
+	}
+	return ip.String()
+}
+
 func LoggerMiddleware(logger *slog.Logger) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -58,11 +75,12 @@ func LoggerMiddleware(logger *slog.Logger) func(http.Handler) http.Handler {
 				slog.Group("request",
 					"method", r.Method,
 					"uri", r.URL.RequestURI(),
+					"ip", redactIP(r.RemoteAddr),
 					// "request_body_bytes", reqRec.bytesRead, // not really useful atm - always 0 since no current endpoint needs a req body
 				),
 				slog.Group("response",
-					"response_status", respRec.statusCode,
-					"response_body_bytes", respRec.bytesWritten,
+					"status", respRec.statusCode,
+					"body_bytes", respRec.bytesWritten,
 				),
 			}
 			logger.Info("Served request", attrs...)
